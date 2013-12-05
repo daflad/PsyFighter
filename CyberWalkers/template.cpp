e08237ada780f037b7c23ce33c7fc48a54a4c47e
Stephen John Russell <stephenjohnrussell@me.com>
2013-12-05 06:02:15 +0000

    going to bed

--- CyberWalkers/template.cpp
+++ CyberWalkers/template.cpp
@@ -1,5 +1,4 @@
-
-#include "GL/glew.h"
+#include <GL/glew.h>
 #include <GLUT/glut.h>
 
 #include "template.h"
@@ -12,20 +11,20 @@
 
 BMPClass bmp;
 GLuint texture_id;
-GLfloat LightAmbient[]=		{ 0.5f, 0.5f, 0.5f, 1.0f };
-GLfloat LightDiffuse[]=		{ 1.0f, 1.0f, 1.0f, 1.0f };
-GLfloat LightPosition[]=	{ 1.0f, 1.0f, 2.0f, 1.0f };
+GLfloat LightAmbient[]=                { 0.5f, 0.5f, 0.5f, 1.0f };
+GLfloat LightDiffuse[]=                { 1.0f, 1.0f, 1.0f, 1.0f };
+GLfloat LightPosition[]=        { 1.0f, 1.0f, 2.0f, 1.0f };
 
 GLuint vboId = 0;           // ID of VBO for vertex arrays
 
-GLfloat	yrot;				// Y Rotation
+GLfloat        yrot;                                // Y Rotation
 
 long cur_fps=0;
 long old_time=0;
 long old_fps=0;
 
 /* This is where you put all your OpenGL drawing commands */
-void display(void)									// Here's Where We Do All The Drawing
+void display(void)                                                                        // Here's Where We Do All The Drawing
 {
     glBindBufferARB(GL_ARRAY_BUFFER_ARB, vboId);
     
@@ -49,14 +48,14 @@ void display(void)									// Here's Where We Do All The Drawing
     glVertexPointer(3, GL_FLOAT, 0, space_craft_001Verts);
     glNormalPointer(GL_FLOAT, 0, space_craft_001Normals);
     glTexCoordPointer(2, GL_FLOAT, 0, space_craft_001TexCoords);
-
+    
     glDrawArrays(GL_TRIANGLES, 0, space_craft_001NumVerts);
-
+    
     
     //Specify the appropriate function call for glDrawElements here:
     // glDrawElements(GL_TRIANGLES,3*NUM_TRIANGLES,GL_UNSIGNED_INT,triangles);
     
-	// ----------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------
     
     glPopMatrix();
     
@@ -78,15 +77,15 @@ void display(void)									// Here's Where We Do All The Drawing
     
     
     
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear Screen And Depth Buffer
-	glLoadIdentity();									// Reset The Current Modelview Matrix
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);        // Clear Screen And Depth Buffer
+    glLoadIdentity();                                                                        // Reset The Current Modelview Matrix
     
     //gluLookAt (300.0, 10.0, 50.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
     glutSwapBuffers();      //swaps the front and back buffers
 }
 
 /* Initialisation routine - acts like your typical constructor in a Java program. */
-void setup(void)										// All Setup For OpenGL Goes Here
+void setup(void)                                                                                // All Setup For OpenGL Goes Here
 {
     // call glewInit() to initialize the OpenGL extension entry points.
     GLenum err = glewInit();
@@ -115,67 +114,67 @@ void setup(void)										// All Setup For OpenGL Goes Here
     glBufferSubDataARB(GL_ARRAY_BUFFER_ARB, 0, sizeof(space_craft_001Verts), space_craft_001Verts);                             // copy vertices starting from 0 offest
     glBufferSubDataARB(GL_ARRAY_BUFFER_ARB, sizeof(space_craft_001Verts), sizeof(space_craft_001Normals), space_craft_001Normals);                // copy normals after vertices
     
-	glShadeModel(GL_SMOOTH);							// Enable Smooth Shading
-	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);				// Black Background
-	glClearDepth(1.0f);									// Depth Buffer Setup
-	glEnable(GL_DEPTH_TEST);							// Enables Depth Testing
-	glDepthFunc(GL_LEQUAL);								// The Type Of Depth Testing To Do
-	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);	// Really Nice Perspective Calculations
+    glShadeModel(GL_SMOOTH);                                                        // Enable Smooth Shading
+    glClearColor(0.0f, 0.0f, 0.0f, 0.5f);                                // Black Background
+    glClearDepth(1.0f);                                                                        // Depth Buffer Setup
+    glEnable(GL_DEPTH_TEST);                                                        // Enables Depth Testing
+    glDepthFunc(GL_LEQUAL);                                                                // The Type Of Depth Testing To Do
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);        // Really Nice Perspective Calculations
     glEnable(GL_LIGHTING);
     
     // Set up  the light
-  	glLightfv(GL_LIGHT0, GL_AMBIENT, LightAmbient);		// Setup The Ambient Light
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, LightDiffuse);		// Setup The Diffuse Light
-	glLightfv(GL_LIGHT0, GL_POSITION,LightPosition);	// Position The Light
-	glEnable(GL_LIGHT0);								// Enable Light
-//    glutTimerFunc(TIMERDELAY, updateScene, 0);
-//    glDepthRange(0, 1);
-//    glutKeyboardFunc(keyInput);
-//	glShadeModel(GL_SMOOTH);							// Enable Smooth Shading
-//	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);				// Black Background
-//	glClearDepth(1.0f);									// Depth Buffer Setup
-//	glEnable(GL_DEPTH_TEST);							// Enables Depth Testing
-//	glDepthFunc(GL_LEQUAL);								// Type of depth testing to do
-//	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);	// Really Nice Perspective Calculations
-//    std::string f = "/Users/stephenjohnrussell/dev/CyberWalkers/CyberWalkers/spaceship_panel_texture_by_dactilardesign-d4v9zb4.bmp";
-//    glEnable ( GL_TEXTURE_2D );
-//    glGenTextures (1, &texture_id);
-//	BMPLoad(f,bmp);
-//    glBindTexture ( GL_TEXTURE_2D, texture_id );
-//	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-//	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-//	glTexImage2D(GL_TEXTURE_2D,0,3,bmp.width,bmp.height,0,GL_RGB,GL_UNSIGNED_BYTE,bmp.bytes);
-//    glLightfv(GL_LIGHT1, GL_AMBIENT, LightAmbient);		// Setup The Ambient Light
-//	glLightfv(GL_LIGHT1, GL_DIFFUSE, LightDiffuse);		// Setup The Diffuse Light
-//	glLightfv(GL_LIGHT1, GL_POSITION,LightPosition);	// Position The Light
-//	glEnable(GL_LIGHT1);								// Enable Light One
-//    
+    glLightfv(GL_LIGHT0, GL_AMBIENT, LightAmbient);                // Setup The Ambient Light
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, LightDiffuse);                // Setup The Diffuse Light
+    glLightfv(GL_LIGHT0, GL_POSITION,LightPosition);        // Position The Light
+    glEnable(GL_LIGHT0);                                                                // Enable Light
+    //    glutTimerFunc(TIMERDELAY, updateScene, 0);
+    //    glDepthRange(0, 1);
+    //    glutKeyboardFunc(keyInput);
+    //        glShadeModel(GL_SMOOTH);                                                        // Enable Smooth Shading
+    //        glClearColor(0.0f, 0.0f, 0.0f, 0.5f);                                // Black Background
+    //        glClearDepth(1.0f);                                                                        // Depth Buffer Setup
+    //        glEnable(GL_DEPTH_TEST);                                                        // Enables Depth Testing
+    //        glDepthFunc(GL_LEQUAL);                                                                // Type of depth testing to do
+    //        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);        // Really Nice Perspective Calculations
+    //    std::string f = "/Users/stephenjohnrussell/dev/CyberWalkers/CyberWalkers/spaceship_panel_texture_by_dactilardesign-d4v9zb4.bmp";
+    //    glEnable ( GL_TEXTURE_2D );
+    //    glGenTextures (1, &texture_id);
+    //        BMPLoad(f,bmp);
+    //    glBindTexture ( GL_TEXTURE_2D, texture_id );
+    //        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    //        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    //        glTexImage2D(GL_TEXTURE_2D,0,3,bmp.width,bmp.height,0,GL_RGB,GL_UNSIGNED_BYTE,bmp.bytes);
+    //    glLightfv(GL_LIGHT1, GL_AMBIENT, LightAmbient);                // Setup The Ambient Light
+    //        glLightfv(GL_LIGHT1, GL_DIFFUSE, LightDiffuse);                // Setup The Diffuse Light
+    //        glLightfv(GL_LIGHT1, GL_POSITION,LightPosition);        // Position The Light
+    //        glEnable(GL_LIGHT1);                                                                // Enable Light One
+    //
     // set input data to arrays
-//    glVertexPointer(3, GL_FLOAT, 0, space_craft_001Verts);
-//    glNormalPointer(GL_FLOAT, 0, space_craft_001Normals);
-//    glTexCoordPointer(2, GL_FLOAT, 0, space_craft_001TexCoords);
-
+    //    glVertexPointer(3, GL_FLOAT, 0, space_craft_001Verts);
+    //    glNormalPointer(GL_FLOAT, 0, space_craft_001Normals);
+    //    glTexCoordPointer(2, GL_FLOAT, 0, space_craft_001TexCoords);
+    
 }
 
 /* Tells the program how to resize your OpenGL Window */
 void resize(int width, int height)
 {
     
-	if (height==0)										// Prevent a divide by zero by
-	{										            // making height equal one
-		height=1;
-	}
+    if (height==0)                                                                                // Prevent a divide by zero by
+    {                                                                                            // making height equal one
+        height=1;
+    }
     
-	glViewport(0,0,(GLsizei)width,(GLsizei)height);		// Set viewport size to be entire OpenGL window
+    glViewport(0,0,(GLsizei)width,(GLsizei)height);                // Set viewport size to be entire OpenGL window
     
-	glMatrixMode(GL_PROJECTION);						// Select the Projection Matrix
-	glLoadIdentity();									// Reset the Projection Matrix
+    glMatrixMode(GL_PROJECTION);                                                // Select the Projection Matrix
+    glLoadIdentity();                                                                        // Reset the Projection Matrix
     
-	// Calculate The Aspect Ratio Of The Window
-	gluPerspective(45.0f,(GLfloat)width/(GLfloat)height,0.1f,100.0f);
+    // Calculate The Aspect Ratio Of The Window
+    gluPerspective(45.0f,(GLfloat)width/(GLfloat)height,0.1f,100.0f);
     
-	glMatrixMode(GL_MODELVIEW);							// Select the Modelview Matrix
-	glLoadIdentity();									// Reset the Modelview Matrix
+    glMatrixMode(GL_MODELVIEW);                                                        // Select the Modelview Matrix
+    glLoadIdentity();                                                                        // Reset the Modelview Matrix
 }
 
 /* Keyboard input processing routine */
@@ -214,6 +213,6 @@ void CalculateFPS()
         old_time =getTimer();
     }
     
-   // sprintf(temp,"Bunny mesh using vertex buffer objects: %d triangles; %d fps",NUM_TRIANGLES, old_fps);
+    // sprintf(temp,"Bunny mesh using vertex buffer objects: %d triangles; %d fps",NUM_TRIANGLES, old_fps);
     glutSetWindowTitle(temp);
 }
