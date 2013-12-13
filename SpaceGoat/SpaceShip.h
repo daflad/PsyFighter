c7b06a6e4055ade71d20f29f87e0c0b8423030ba
Stephen John Russell <stephenjohnrussell@me.com>
2013-12-13 01:57:41 +0000

    New files too ;)

---
+++ SpaceGoat/SpaceShip.h
@@ -0,0 +1,34 @@
+//
+//  SpaceShip.h
+//  CyberWalkers
+//
+//  Created by Stephen John Russell on 09/12/2013.
+//  Copyright (c) 2013 Stephen John Russell. All rights reserved.
+//
+
+#ifndef __CyberWalkers__SpaceShip__
+#define __CyberWalkers__SpaceShip__
+
+#include <iostream>
+#include "GL/glew.h"
+#include <GLUT/glut.h>
+#include "BMPLoader.h"
+
+class SpaceShip {
+
+public:
+
+    float x, y, z, dist;
+    
+    BMPClass bmp;
+    GLuint texture_id;
+    
+    unsigned int SpaceShipObjectNumVerts;
+    
+    SpaceShip();
+    void setup(float *verts, float *norms, float *texts, unsigned int numV);
+    void draw();
+    
+};
+
+#endif /* defined(__CyberWalkers__SpaceShip__) */
