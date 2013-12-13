16773841f9e0f84e00f55212f9a4a4ab4b48595b
Stephen John Russell <stephenjohnrussell@me.com>
2013-12-13 01:47:15 +0000

    Name change complete

---
+++ SpaceGoat/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  Three-Dimentional Gasket
+//
+//  Created by Liang Sun on 1/6/13.
+//  Copyright (c) 2013 Liang Sun. All rights reserved.
+//
+#include "template.h"
+
+
+
+int main(int argc, char** argv)
+{
+    printf("Application Started");
+    glutInit(&argc, argv);
+    
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE | GLUT_DEPTH);
+    
+    glutInitWindowSize(1280, 800);
+    glutInitWindowPosition(20, 20);
+    glutCreateWindow("GLUT Program");
+    
+    glutDisplayFunc(display);
+    
+    setup();
+    glutMainLoop();
+    return 0;
+}
